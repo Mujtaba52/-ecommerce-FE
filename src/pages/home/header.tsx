dba5243f3702a42d6839e83afeb4aa6d2163e73c
Mujtaba Hassan <mujhassan786@gmail.com>
2024-10-20 22:11:19 +0300

    add header bar

--- src/pages/home/header.tsx
+++ src/pages/home/header.tsx
@@ -1,7 +1,39 @@
 
 const Header = ()=>{
     return (<>
-        Header
+        <div className="border-b">
+            <div className="flex ml-[135px] mt-10 mb-4">
+                <div className="font-Inter font-bold text-2xl mr-48" >
+                    Exclusive
+                </div>
+                <div className="font-Poppins font-normal text-base flex justify-around gap-x-12 mr-36">
+                    <button>
+                        Home
+                    </button>
+                    <button>
+                        Contact
+                    </button>
+                    <button>
+                        About
+                    </button>
+                    <button>
+                        Sign Up
+                    </button>
+                </div>
+                <div className="flex items-center space-x-6">
+                    <div className="flex items-center bg-transparent bg-[#F5F5F5] pl-5 pr-3 py-2 w-64 rounded" >
+                        <input
+                            type="text"
+                            placeholder="What are you looking for?"
+                            className="bg-transparent w-full"
+                        />
+                        <img src="src/assets/images/svg/landingPage/searchBarIcon.svg"  className="w-6 h-6 cursor-pointer"/>
+                    </div>
+                    <img  src="src/assets/images/svg/landingPage/heart.svg" alt="Heart Img" className="w-7 h-7 cursor-pointer" />
+                    <img src="src/assets/images/svg/landingPage/cart.svg" alt="Cart Img" className="w-8 h-8 cursor-pointer" />
+                </div>
+            </div>
+        </div>
     </>
     );
 }
