efff6a9ec2f6b66890b6e3a1be7a116a6f361578
Mujtaba Hassan <mujhassan786@gmail.com>
2024-10-23 06:11:12 +0300

    update carousel to add arrows on the top right corner & remove heading from the carousel component

--- src/components/common/headingWithBadge.tsx
+++ src/components/common/headingWithBadge.tsx
@@ -1,17 +1,23 @@
 
 interface HeadingWithBadgeProps {
-    heading: string;
+    subHeading: string;
+    mainHeading: string;
   }
 
-const HeadingWithBadge = ({heading}:HeadingWithBadgeProps)=>{
+const HeadingWithBadge = ({subHeading, mainHeading}:HeadingWithBadgeProps)=>{
     return (
-        <div className="flex items-center gap-4 mb-6">
-            <div className="w-5 h-10 bg-[#DB4444] rounded ">
+        <>
+            <div className="flex items-center gap-4 mb-6">
+                <div className="w-5 h-10 bg-[#DB4444] rounded ">
+                </div>
+                <div className="text-[#DB4444] font-Poppins font-semibold text-base">
+                    {subHeading}
+                </div>
             </div>
-            <div className="text-[#DB4444] font-Poppins font-semibold text-base">
-                {heading}
+            <div className="h-12 font-Poppins font-semibold text-4xl mb-6">
+                {mainHeading}
             </div>
-        </div>
+        </>
         
     )
 }
--- src/components/common/productCarousel.tsx
+++ src/components/common/productCarousel.tsx
@@ -1,4 +1,6 @@
 import ProductCard from "./productCard";
+import Slider from "react-slick";
+
 
 //TODO:Add this is in the interface folder
 
@@ -18,24 +20,75 @@ interface ProductCarouselProps {
     products: ProductProp[]; 
 }
 
-const ProductCarousel = ({products}:ProductCarouselProps )=>{
-
+  function SampleNextArrow(props: any) {
+    const { onClick } = props;
     return (
-        <div>
-            <div className="h-12 font-Poppins font-semibold text-4xl mb-10">
-                Flash Sales
-            </div>
-             <div className="flex gap-8">
-            {
-                products.map((productProp:ProductProp)=>(
-                    <div key={productProp.id}>
-                        <ProductCard product={productProp}/>
-                    </div>
-                ))
-            }
-            </div>
+      <div
+        style={{
+          display: "block",
+          background: "#F5F5F5",
+          padding: "10px",
+          borderRadius: "50%",
+          cursor: "pointer",
+          position: "absolute",
+          top: "-75px",
+          right: "32px",
+        }}
+        onClick={onClick}
+      >
+       <img src="src/assets/images/svg/landingPage/rightArrow.svg" />
+      </div>
+    );
+  }
+  
+  function SamplePrevArrow(props: any) {
+    const { onClick } = props;
+    return (
+      <div
+        style={{
+          display: "block",
+          background: "#F5F5F5",
+          padding: "10px",
+          borderRadius: "50%",
+          cursor: "pointer",
+          position: "absolute",
+          top: "-75px",
+          right: "96px",
+        }}
+        onClick={onClick}
+      >
+        <img src="src/assets/images/svg/landingPage/leftArrow.svg" />
+      </div>
+    );
+  }
+  
+  const ProductCarousel = ({ products }: ProductCarouselProps) => {
+    var settings = {
+      dots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      nextArrow: <SampleNextArrow />,
+      prevArrow: <SamplePrevArrow />
+      ,
+    };
+  
+    return (
+      <div style={{ position: "relative", paddingTop: "0px" }}>
+        <div className="max-w-[1212px]">
+            <Slider {...settings}>
+            {products.map((productProp: ProductProp) => (
+                <div key={productProp.id}>
+                <ProductCard product={productProp} />
+                </div>
+            ))}
+            </Slider>
         </div>
-    )
-}
-
-export default ProductCarousel
+       
+      </div>
+    );
+  };
+  
+  export default ProductCarousel;
+  
