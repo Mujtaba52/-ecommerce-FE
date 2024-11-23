4679cfcde3971d2f7338d7b4484dd2e5c81e6a85
Mujtaba Hassan <mujhassan786@gmail.com>
2024-11-23 22:54:16 +0200

    feat:chatbot add action provider for chatBot

---
+++ src/components/chatBot/ActionProvider.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { IMessageOptions } from "react-chatbot-kit/build/src/interfaces/IMessages";
+
+const ActionProvider = ({ createChatBotMessage, setState, children }: {
+  createChatBotMessage: (
+    message: string,
+    options: IMessageOptions
+  ) => {
+    loading: boolean;
+    widget?: string;
+    delay?: number;
+    payload?: any;
+    message: string;
+    type: string;
+    id: number;
+  };
+  setState: any;
+  children: any;
+}) => {
+  return (
+    <div>
+      {React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          actions: {},
+        });
+      })}
+    </div>
+  );
+};
+
+export default ActionProvider;
