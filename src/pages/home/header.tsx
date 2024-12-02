9ca3a0068ab4d41ac705336d47aff408557e8e98
Mujtaba Hassan <mujhassan786@gmail.com>
2024-12-02 23:25:36 +0200

    feat : integrate search bar

--- src/pages/home/header.tsx
+++ src/pages/home/header.tsx
@@ -1,6 +1,7 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store/store.ts"
+import SearchBar from "./SearchBar";
 
 const Header = ()=>{
     const cartItems = useSelector((state: RootState) => state.cart)
@@ -11,9 +12,9 @@ const Header = ()=>{
     const cartItemCount = cartItems.length;
     return (<>
         <div className="border-b">
-            <div className="flex ml-[135px] mt-6 mb-2 items-center">
+            <div className="flex ml-[135px] mt-2 mb-2 items-center">
                 <Link to="/" className="font-Inter font-bold text-2xl mr-48" >
-                    Exclusive
+                    <img src= "public/assets/images/png/landingPage/EIcon.png" className="w-20 h-20"/>
                 </Link>
                 <div className=" font-semibold text-sm flex justify-around gap-x-12 mr-36">
                     <Link to="/" className="hover:underline underline-offset-4">
@@ -30,14 +31,7 @@ const Header = ()=>{
                     </Link>
                 </div>
                 <div className="flex items-center space-x-6">
-                    <div className="flex items-center bg-transparent bg-[#e1e1e1] pl-5 pr-3 py-2 w-72 rounded border-2" >
-                        <input
-                            type="text"
-                            placeholder="Search items..."
-                            className="bg-transparent w-full focus:outline-none"
-                        />
-                        <img src="/assets/images/svg/landingPage/searchBarIcon.svg"  className="w-6 h-6 cursor-pointer"/>
-                    </div>
+                    <SearchBar />
                     <img  src="/assets/images/svg/landingPage/heart.svg" alt="Heart Img" className="w-7 h-7 cursor-pointer" />
                     <div className="relative">
                         <img onClick={handleViewCartClick} src="/assets/images/svg/landingPage/cart.svg" alt="Cart Img" className="w-8 h-8 cursor-pointer" />
